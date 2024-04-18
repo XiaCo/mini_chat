6094e0b9d59bfb5bdc9d2821687eadebafb2a9e7
xia-mac <xia-mac@192.168.1.2>
2024-04-18 15:51:11 +0800

    update frame

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,5 +1,29 @@
-use mini_chat::{server};
+use std::fmt::Write;
 
-fn main() {
-    println!("Hello, mini-chat server!");
+use bytes::{Buf, BytesMut};
+use mini_chat::{server};
+use clap::Parser;
+use tokio::{net::TcpListener, signal};
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct ServerConf {
+    #[arg(long)]
+    host: String,
+
+    #[arg(short, long, default_value_t = 80)]
+    port: u16,
 }
+
+#[tokio::main]
+pub async fn main() {
+    // let sc = ServerConf::parse();
+    
+    // Bind a TCP listener
+    // let listener = TcpListener::bind(&format!("{}:{}", sc.host, sc.port)).await?;
+
+    // server::run(listener, signal::ctrl_c()).await;
+}
+
+
--- src/connection.rs
+++ src/connection.rs
@@ -1,10 +1,11 @@
-use std::io::{self, Cursor, ErrorKind};
+use std::array;
+use std::io::{self, Cursor, ErrorKind, Read};
 use std::sync::atomic::AtomicU32;
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 use tokio::net::TcpStream;
 
-use bytes::{Buf, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 
 use crate::{Message};
 
@@ -12,8 +13,6 @@ pub type Frame = Vec<u8>;
 static FRAME_ID: AtomicU32 = AtomicU32::new(0);
 
 pub const MAX_FRAME_LENGTH: usize = 1024 * 4;
-pub const HEADER_KEY_LENGTH: &str = "body-length";
-pub const HEADER_FRAME_ID: &str = "frame-id";
 
 // Connection.buffer max size
 const MAX_BUFFER_LENGTH: usize = 1024 * 8;
@@ -23,6 +22,8 @@ pub struct Connection {
     conn: BufWriter<TcpStream>,
 
     buffer: BytesMut,
+
+    stream_id: AtomicU32,
 }
 
 #[derive(PartialEq)]
@@ -47,6 +48,7 @@ impl Connection {
         Connection {
             conn: BufWriter::new(stream),
             buffer: BytesMut::with_capacity(MAX_BUFFER_LENGTH),
+            stream_id: AtomicU32::new(0),
         }
     }
 
@@ -75,63 +77,32 @@ impl Connection {
     }
 
     fn parse_frame(&mut self) -> Result<Message, Error> {
-        // 读一行
-        let buf = Cursor::new(&self.buffer[..]);
-        let buf_length = buf.get_ref().len();
-        let line_u8 = Connection::get_line(buf, MAX_FRAME_LENGTH)?;
-        let line = String::from_utf8_lossy(line_u8);
-
-        // 解析 header
-        let headers: Vec<&str> = line.split(' ').collect();
-        let mut body_length: usize = 0;
-        for i in 0..headers.len() {
-            let item = headers[i];
-            if item == HEADER_KEY_LENGTH && i < headers.len() - 1 {
-                body_length = headers[i - 1].parse()?
-            }
+        // 读协议头
+        let mut buf = Cursor::new(&self.buffer[..]);
+        let frame_length = buf.get_u32();
+        let stream_id = buf.get_u32();
+        if buf.remaining() < frame_length as usize {
+            return Err(Error::Incomplete)
         }
+        
+        let frame_payload = &buf.get_ref()[..frame_length as usize];
 
         // 读 body 并反序列化
-        let frame_length = line_u8.len() + body_length; // header + body
-        if body_length > 0 && buf_length > frame_length {
-            let body = &self.buffer[line_u8.len()..body_length];
-            let msg: Message = serde_json::from_slice(body)?;
+        let msg: Message = serde_json::from_slice(frame_payload)?;
 
-            self.buffer.advance(frame_length);
-            return Ok(msg);
-        }
-
-        Err(Error::Incomplete)
-    }
-
-    fn get_line(src: Cursor<&[u8]>, max_read: usize) -> Result<&[u8], Error> {
-        for i in 0..src.get_ref().len() {
-            if i == max_read {
-                return Err(Error::FrameOverflow);
-            }
-            if src.get_ref()[i] == b'\n' {
-                return Ok(&src.get_ref()[..i]);
-            }
-        }
-
-        Err(Error::Incomplete)
+        self.buffer.advance(4+4+frame_length as usize);
+        return Ok(msg);
     }
 
     // 封装通信包，填充字节流长度，帧id
-    fn new_frame(v: Message) -> Result<Frame, io::Error> {
+    fn new_frame(&mut self, v: Message) -> Result<Frame, io::Error> {
         let body = serde_json::to_vec(&v).unwrap();
-        let headers = format!(
-            "{}:{} {}:{}\n",
-            HEADER_KEY_LENGTH,
-            body.len(),
-            HEADER_FRAME_ID,
-            FRAME_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed), // 自增 id
-        );
-        let mut f: Frame = Vec::with_capacity(headers.len() + body.len());
+        let mut f: Frame = Vec::with_capacity(8 + body.len());
     
-        let n1 = io::Write::write(&mut f, headers.as_bytes())?;
-        let n2 = io::Write::write(&mut f, &body)?;
-        if (n1 + n2) > MAX_FRAME_LENGTH {
+        let n1 = io::Write::write(&mut f, body.len().to_be_bytes().as_slice())?;
+        let n2 = io::Write::write(&mut f,self.stream_id.fetch_add(2, std::sync::atomic::Ordering::Relaxed).to_be_bytes().as_slice())?;
+        let n3 = io::Write::write(&mut f, &body)?;
+        if (n1 + n2 + n3) > MAX_FRAME_LENGTH {
             return Err(io::Error::new(ErrorKind::InvalidData, "too large frame"));
         }
     
@@ -139,7 +110,7 @@ impl Connection {
     }
 
     pub async fn write_frame(&mut self, msg: Message) -> io::Result<()> {
-        let f = Connection::new_frame(msg)?;
+        let f = self.new_frame(msg)?;
         self.conn.write(&f).await?;
 
         self.conn.flush().await
